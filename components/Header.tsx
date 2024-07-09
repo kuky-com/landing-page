4a17faea3da39b3333822fdd67767f290784f065
eastlead <eastlead.park@gmail.com>
2024-07-09 22:59:25 +0900

    Updated the box text
    1. Updated svg to dom unit
    2. Updated the text

--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
     };
 
     return (
-        <header className="p-4 flex justify-between items-center">
+        <header className="p-4 flex justify-between items-center mb-24">
             <div className="flex items-center">
                 <Image src="/kuky-icon.svg" alt="Kuky Icon" width={34} height={34} />
                 <p className="ml-2 font-lexend text-[10px] font-bold leading-[12.5px] text-left text-black">
@@ -38,8 +38,13 @@ const Header: React.FC = () => {
                 <div className="wiggle-animation mr-6 relative">
                     <Image src="/notification-icon.svg" alt="Notification Icon" width={24} height={24} />
                 </div>
-                <div className="absolute -bottom-12 left-3 transform -translate-x-1/2">
-                    <Image src="/notification-cloud.svg" alt="Notification Cloud" width={133} height={41} />
+                <div className="absolute -bottom-24 left-3 transform -translate-x-1/2">
+                    <div className="inline-block relative">
+                        <div className="bg-white text-gray-800 px-4 py-2 rounded-md shadow-lg z-10 border border-gray-200">
+                            <span className="text-sm font-medium">Sign up to get 3 months free access</span>
+                            <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-b-8 border-l-transparent border-r-transparent border border-gray-100"></div>
+                        </div>
+                    </div>
                 </div>
                 <div className="relative">
                     <Image src="/star-button.png" alt="Star Button" width={120} height={32} />

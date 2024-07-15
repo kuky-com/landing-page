fb93ca2384f6d5128b49c7cbe14f997dae5dcf95
Ben <hsb1007@gmail.com>
2024-07-15 14:53:33 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -51,11 +51,15 @@ export default function Home() {
       <Header />
 
       <main className="flex-grow flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8">
-        <h1 className="font-lexend text-4xl sm:text-5xl md:text-6xl lg:text-[80px] font-extrabold leading-[1.1em] sm:leading-[86px] tracking-[0.002em] text-center mb-8 sm:mb-6 bg-clip-text text-transparent bg-gradient-to-b from-[#686868] via-[#686868] to-[#202020] px-4 sm:px-0">
+        <h1 className="font-lexend text-4xl sm:text-5xl md:text-6xl lg:text-[80px] font-extrabold leading-[1.1em] sm:leading-[86px] tracking-[0.002em] text-center mb-6 sm:mb-6 bg-clip-text text-transparent bg-gradient-to-b from-[#686868] via-[#686868] to-[#202020] px-4 sm:px-0">
           <span className="mb-2 sm:mb-0 block leading-[1.2em]">Talk to Kuky,</span>
           <span className="block leading-[1.2em] mt-[-0.1em]">find your match</span>
         </h1>
-
+        <h3 class="font-lexend text-2xl sm:text-3xl md:text-2xl lg:text-[70px] font-extrabold leading-[1.1em] sm:leading-[86px] tracking-[0.002em] text-center mb-6 sm:mb-6 bg-clip-text text-transparent bg-gradient-to-b from-[#686868] via-[#686868] to-[#202020] px-4 sm:px-0"><span class="mb-2 sm:mb-0 block leading-[1.2em]">Sign up for private beta access</span></h3>
+        <p className="text-sm text-center mt-0 text-[#666666] mb-6">
+          Want to learn more about Kuky? Watch <button onClick={() => setIsVideoOpen(true)} className=" underline">our video</button> 🕹️
+        </p>
+        
         <p className="font-roboto text-base font-light leading-[26px] text-center text-[rgba(102,102,102,1)] max-w-3xl mx-auto mb-16 sm:mb-12 px-6 sm:px-4">
           Hold tight! Kuky is on its way to revolutionize how you meet and connect with people who share your interests. Whether it&apos;s finding a jogging buddy or a study partner, Kuky&apos;s got you covered.
         </p>

2753f11397ac825075dbbddc4fe2bc59bbe4a113
Ben <hsb1007@gmail.com>
2024-08-05 15:28:04 +1000

    google tag manager hot fix

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,14 +2,20 @@ import Script from 'next/script'
 
 const GoogleAnalytics = () => (
     <>
-        <Script async src={`https://www.googletagmanager.com/gtag/js?id${process.env.NEXT_PUBLIC_GA_ID}`} />
+       
+
         <Script id="google-analytics">
             {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-      `}
+            !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+            var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+            ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+
+            ttq.load('CQO32KBC77U65T3JBKU0');
+            ttq.page();
+        }(window, document, 'ttq');
+        `}
         </Script>
     </>
 )

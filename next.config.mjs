7ec0e5e80fba989f434deb8d9e04ec600cd631b6
Tung Mai <tungmt@outlook.com>
2025-04-09 23:39:17 +0700

    try to fix cannot open store when open link on phone issue

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
     async headers() {
         return [
             {
+                source: '/.well-known/apple-app-site-association',
+                headers: [
+                  {
+                    key: 'Content-Type',
+                    value: 'application/json',
+                  },
+                ],
+            },
+            {
                 source: '/:path*',
                 headers: [
                     {
